697297eee61799fabb45f45d1fa03d66fe525096
Tausif Fardin <tausiffardin4@gmail.com>
2022-09-12 13:33:04 +0600

    added connect.py

--- beacon_scanner.py
+++ beacon_scanner.py
@@ -1,18 +1,23 @@
-# To discover Bluetooth devices that can be connected to:
-
 import asyncio
-from bleak import BleakScanner
+from bleak import BleakScanner, BleakClient
 
-def detection_callback(device, advertisement_data):
-    print(device.address, "RSSI:", device.rssi, advertisement_data)
 
 async def main():
-    scanner = BleakScanner(detection_callback)
-    await scanner.start()
-    await asyncio.sleep(15.0)
-    await scanner.stop()
-
-    for d in scanner.discovered_devices:
+    devices = await BleakScanner.discover()
+    for d in devices:
         print(d)
+
+asyncio.run(main())
+
+
+import asyncio
+from bleak import BleakScanner, BleakClient
+
+async def main():
+    devices = await BleakScanner.discover()
+    for d in devices:
+        if (d.address) == 'E8:21:16:DE:40:03':
+            print(d.address)
+            print('Found it')
 
 asyncio.run(main())
---
+++ connect.py
@@ -0,0 +1,14 @@
+import asyncio
+
+#Connect to a Bluetooth device and read its model number:
+from bleak import BleakClient
+
+address = "E8:21:16:DE:40:03"
+MODEL_NBR_UUID = "0000feaa-0000-1000-8000-00805f9b34fb"
+
+async def main(address):
+    async with BleakClient(address) as client:
+        model_number = await client.read_gatt_char(MODEL_NBR_UUID)
+        print("Model Number: {0}".format("".join(map(chr, model_number))))
+
+asyncio.run(main(address))
