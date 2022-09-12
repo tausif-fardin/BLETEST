4a921880d8c5365d8aa6798ce8ce128c0f4c51ed
Tausif Fardin <tausiffardin4@gmail.com>
2022-09-12 15:04:40 +0600

    discover updated

--- discover.py
+++ discover.py
@@ -1,23 +1,5 @@
-# import asyncio
-
-# #Connect to a Bluetooth device and read its model number:
-# from bleak import BleakClient
-
-# address = "E8:21:16:DE:40:03"
-# MODEL_NBR_UUID = "0000feaa-0000-1000-8000-00805f9b34fb"
-
-# async def main(address):
-#     async with BleakClient(address) as client:
-#         model_number = await client.read_gatt_char(MODEL_NBR_UUID)
-#         print("Model Number: {0}".format("".join(map(chr, model_number))))
-
-# asyncio.run(main(address))
-
-
 import asyncio
-from dis import dis
 from uuid import UUID
-import math
 
 from construct import Array, Byte, Const, Int8sl, Int16ub, Struct
 from construct.core import ConstError
@@ -43,17 +25,18 @@ def device_found(
         apple_data = advertisement_data.manufacturer_data[0x004C]
         ibeacon = ibeacon_format.parse(apple_data)
         uuid = UUID(bytes=bytes(ibeacon.uuid))
-        print(ibeacon.power, device.rssi)
+        #print(ibeacon.power, device.rssi)
         ratio =((int(ibeacon.power)-int(device.rssi))/20)
-        distance= math.pow(ratio,10)
+        distance= ratio**10
         
-        
-        print(f"UUID     : {uuid}")
-        print(f"Major    : {ibeacon.major}")
-        print(f"Minor    : {ibeacon.minor}")
-        print(f"TX power : {ibeacon.power} dBm")
-        print(f"RSSI     : {device.rssi} dBm")
-        print(f"DISTANCE : {distance} m")
+        print(f"""
+UUID     : {uuid}
+Major    : {ibeacon.major}
+Minor : {ibeacon.minor}
+Tx Power : {ibeacon.power} dBm
+RSSI : {device.rssi} dBm
+Distance : {distance} m
+        """)
         print(47 * "-")
     except KeyError:
         # Apple company ID (0x004c) not found
@@ -70,7 +53,7 @@ async def main():
 
     while True:
         await scanner.start()
-        await asyncio.sleep(2.0)
+        await asyncio.sleep(5.0)
         await scanner.stop()
 
 
