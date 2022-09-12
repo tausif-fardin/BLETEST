b7146beffb9c0fb99393829b7c0bee7462e0d996
Tausif Fardin <tausiffardin4@gmail.com>
2022-09-12 13:52:32 +0600

    beacon scanner updated

--- beacon_scanner.py
+++ beacon_scanner.py
@@ -1,23 +1,27 @@
+# import asyncio
+# from bleak import BleakScanner, BleakClient
+
+
+# async def main():
+#     devices = await BleakScanner.discover()
+#     for d in devices:
+#         print(d)
+
+# asyncio.run(main())
+
+
 import asyncio
 from bleak import BleakScanner, BleakClient
-
 
 async def main():
     devices = await BleakScanner.discover()
     for d in devices:
+        # printing device list
         print(d)
-
-asyncio.run(main())
-
-
-import asyncio
-from bleak import BleakScanner, BleakClient
-
-async def main():
-    devices = await BleakScanner.discover()
-    for d in devices:
+        #getting the details of specific device with its address
         if (d.address) == 'E8:21:16:DE:40:03':
-            print(d.address)
+            print(d.address, d.rssi,d.metadata["uuids"])
             print('Found it')
 
 asyncio.run(main())
+
